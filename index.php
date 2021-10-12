b2018b5bd1268d1e4b7a53899568be8e218571ef
skidi-papap <92352524+skidi-papap@users.noreply.github.com>
2021-10-12 20:22:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
 <html>
 <head>
-    <link rel="stylesheet" href="style.css" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
     <body>
-        <div class="form-left-decoration"></div>
-        <div class="form-right-decoration"></div>
-        <div class="circle"></div>
-        <div class="form-inner">
-            <h1>Aduan Siber</h1>
-            <form action="bot_form.php" method="POST">    
+        <form action="bot_form.php" method="POST">    
+            <div class="form-left-decoration"></div>
+            <div class="form-right-decoration"></div>
+            <div class="circle"></div>
+            <div class="form-inner">
+                <h1>Aduan Siber</h1>
                 <input type="text" class="form-control" name="nama" placeholder="Nama">
                 <input type="text" class="form-control" name="uname" placeholder="email">
                 <textarea rows="5" type="text" class="form-control" name="teks" placeholder="aduan"></textarea>
-                <input type="submit" class="btn btn-primary" value="Submit">
-            </form>
-        </div>
+                <input type="submit" class="btn btn-primary" value="Submit"> 
+            </div>
+        </form>
     </body>
 </html>
