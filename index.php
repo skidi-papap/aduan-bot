ae8111ce7a48b6890978faa207024481d403dd37
skidi-papap <92352524+skidi-papap@users.noreply.github.com>
2021-10-12 19:29:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+    <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -7,20 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
     <body>
-        <div class"col-lg-1">
-            <form action="bot_form.php" method="POST">
-                <div class="form-group">
-                <label for="name">Nama :</label>    
-                <input type="text" class="form-control" name="nama" id="name">
-                </div>
-                <div class="form-group">
-                <label for="email">Email :</label>    
-                <input type="text" class="form-control" name="uname" id="email">
-                </div>
-                <div class="form-group">
-                <label for="aduan">Aduan :</label>
-                <input type="text" class="form-control" name="teks" id="aduan">
-                </div>
+        <div class="form-left-decoration"></div>
+        <div class="form-right-decoration"></div>
+        <div class="circle"></div>
+        <div class="form-inner">
+            <form action="bot_form.php" method="POST">    
+                <input type="text" class="form-control" name="nama" placeholder="Nama">
+                <input type="text" class="form-control" name="uname" placeholder="email">
+                <textarea rows="5" type="text" class="form-control" name="teks" placeholder="aduan">
                 <input type="submit" class="btn btn-primary" value="Submit">
             </form>
         </div>
