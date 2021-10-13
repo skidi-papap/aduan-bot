0c39f2dba461f6f9241ed220df49638c74fe6da9
skidi-papap <92352524+skidi-papap@users.noreply.github.com>
2021-10-13 10:47:55 +0700

    Update bot_form.php

--- bot_form.php
+++ bot_form.php
@@ -1,12 +1,17 @@
 <?php
 $token = "2078735666:AAF8MXb1xSQe0qx3kvVQEeErpvCdTNjq4A8";
-$teks = "Nama : " . $_POST['nama'] . "\n" . "Email : " . $_POST['uname'] . "\n" . "Aduan : " . $_POST['teks'];
-$user = $_POST['uname'];
-$data = [
-    'text' => $teks,
-    'chat_id' => '539091338',
-];
+$nama = filter_var($_POST['nama'], FILTER_SANITIZE_STRING);
+$aduan = filter_var($_POST['teks'], FILTER_SANITIZE_STRING);
+if(filter_var($_POST['uname'], FILTER_VALIDATE_EMAIL)) {
+    $teks = "Nama : " . $_nama . "\n" . "Email : " . $_POST['uname'] . "\n" . "Aduan : " . $aduan;
+    $data = [
+        'text' => $teks,
+        'chat_id' => '539091338',
+    ];
 
-$forward = file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?" . http_build_query($data));
-header("Location: index.php");
+    $forward = file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?" . http_build_query($data));
+        header("Location: index.php");
+} else{
+    echo "<script>alert('Email invalid')</script>";
+}
 ?>
