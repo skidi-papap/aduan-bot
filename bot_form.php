65282e57f2778934e3bd8076380cab8fabc63c2d
skidi-papap <92352524+skidi-papap@users.noreply.github.com>
2021-10-13 10:54:47 +0700

    Update bot_form.php

--- bot_form.php
+++ bot_form.php
@@ -3,7 +3,7 @@ $token = "2078735666:AAF8MXb1xSQe0qx3kvVQEeErpvCdTNjq4A8";
 $nama = filter_var($_POST['nama'], FILTER_SANITIZE_STRING);
 $aduan = filter_var($_POST['teks'], FILTER_SANITIZE_STRING);
 if(filter_var($_POST['uname'], FILTER_VALIDATE_EMAIL)) {
-    $teks = "Nama : " . $_nama . "\n" . "Email : " . $_POST['uname'] . "\n" . "Aduan : " . $aduan;
+    $teks = "Nama : " . $nama . "\n" . "Email : " . $_POST['uname'] . "\n" . "Aduan : " . $aduan;
     $data = [
         'text' => $teks,
         'chat_id' => '539091338',
